07c9b0977cf7828a45656c2c55fed1daafe046a5
Tuukka Partanen <tospartanen@gmail.com>
2018-11-20 14:47:11 +0200

    Core works
    Plain working version ready

--- SecretMessagesGUI/src/SecretMessagesGUI.java
+++ SecretMessagesGUI/src/SecretMessagesGUI.java
@@ -4,6 +4,8 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class SecretMessagesGUI extends JFrame {
 	private JTextField textKey;
@@ -11,6 +13,37 @@ public class SecretMessagesGUI extends JFrame {
 	private JTextArea textOut;
 	
 	public String encode(String message, int keyVal) {
+		String output = "";
+		char key = (char) keyVal;
+		for ( int x = 0; x < message.length(); x++ ) {
+            char input = message.charAt(x);
+            if (input >= 'A' && input <= 'Z')
+            {
+                input += key;
+                if (input > 'Z')
+                    input -= 26;
+                if (input < 'A')
+                    input += 26;
+            }
+            else if (input >= 'a' && input <= 'z')
+            {
+                input += key;
+                if (input > 'z')
+                    input -= 26;
+                if (input < 'a')
+                    input += 26;
+            }
+            else if (input >= '0' && input <= '9')
+            {
+                input += (keyVal % 10);
+                if (input > '9')
+                    input -= 10;
+                if (input < '0')
+                    input += 10;
+}
+            output += input;
+        }
+        return output;
     }
 	
 	public SecretMessagesGUI() {
@@ -37,12 +70,24 @@ public class SecretMessagesGUI extends JFrame {
 		getContentPane().add(lblKey);
 		
 		JButton btnEncodedecode = new JButton("Encode/Decode");
+		btnEncodedecode.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			try {
+                String message = textIn.getText();
+                int key = Integer.parseInt( textKey.getText() );
+                String output = encode(message, key);
+                textOut.setText(output);
+			} catch(Exception ex) {
+			}
+			}
+		});
 		btnEncodedecode.setBounds(292, 173, 131, 29);
 		getContentPane().add(btnEncodedecode);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SecretMessagesGUI theApp = new SecretMessagesGUI();
+		theApp.setSize(new java.awt.Dimension(600,400));
+		theApp.setVisible(true);
 	}
 }
